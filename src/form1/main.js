d92b92c84110edf03d52c76b3c5fe434ced3a37e
jiangchangzzz <1015312525@qq.com>
2017-02-27 21:37:43 +0800

    修改为通过添加相应类实现

--- src/form1/main.js
+++ src/form1/main.js
@@ -8,13 +8,15 @@ window.onload=function(){
     check.onclick=function(event){
         var name=form.elements['name'];
         var value=name.value;
+
+        //判断输入内容是否为空
         if(name.value==null || name.value===''){
             message.innerText='姓名不能为空';
-            message.style.color='red';
-            name.style.borderColor='red';
+            name.parentNode.className='form-group danger';
             return;
         }
         
+        //匹配出输入内容中字母和数字的个数
         var length=0;
         var num=/\w+/g;
         var match=null;
@@ -22,21 +24,20 @@ window.onload=function(){
             length+=match[0].length;
         }
 
+        //匹配出输入结果中汉字的个数
         var text=/[\u4e00-\u9fa5]/g;
         while((match=text.exec(value))!==null){
             length+=(match[0].length)*2;
         }
 
+        //判断字符长度是否符合要求
         if(length>16 || length<4){
             message.innerText='长度必须为4~16个字符';
-            message.style.color='red';
-            name.style.borderColor='red';
-            name.select();
+            name.parentNode.className='form-group danger';
         }
         else{
             message.innerText='名称格式正确';
-            message.style.color='green';
-            name.style.borderColor='green';
+            name.parentNode.className='form-group success';
         }
     }
 }
