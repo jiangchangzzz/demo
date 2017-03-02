eea66eb6ec854c2a2dda10bcb8ce590fb960e8b4
jiangchangzzz <1015312525@qq.com>
2017-03-02 21:16:12 +0800

    修改了数据相同时的bug

--- src/js1/main.js
+++ src/js1/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//队列数据模型
 var queue=[];
 var show=document.getElementById('show');
 var num=document.getElementById('num');
@@ -19,27 +20,42 @@ document.getElementById('rightin').onclick=function(){
 }
 
 document.getElementById('leftout').onclick=function(){
-        queue.shift();
+        var value=queue.shift();
         showQueue();
         //使用定时器是为了在函数执行完成，添加DOM节点后再弹框
-        setTimeout(function(){alert(queue.length);},0);
+        setTimeout(function(){alert(value);},0);
 }
 
 document.getElementById('rightout').onclick=function(){
-        queue.pop();
+        var value=queue.pop();
         showQueue();
         //使用定时器是为了在函数执行完成，添加DOM节点后再弹框
-        setTimeout(function(){alert(queue.length);},0);
+        setTimeout(function(){alert(value);},0);
 }
 
 show.onclick=function(event){
-    if(event.target.nodeName.toLowerCase()==='li'){
-        var current=event.target.innerText.trim();
-        queue.splice(queue.indexOf(current),1);
+    var event=event || window.event;
+    var target=event.target || event.srcElement;
+
+    if(target.nodeName.toLowerCase()==='li'){
+        var index=-1;
+        var list=show.childNodes;
+        //找出删除要删除节点的次序
+        for(var i=0,len=list.length;i<len;i++){
+            if(list[i]===target){
+                index=i;
+                break;
+            }
+        }
+        queue.splice(index,1);
         showQueue();
     }
 }
 
+/**
+ * 用队列数据模型渲染视图
+ * 
+ */
 function showQueue(){
     var str='';
     for(var i=0,len=queue.length;i<len;i++){
