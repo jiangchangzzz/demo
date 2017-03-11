27429203169f59aba0c2c7f790f8eb49aedac2f1
jiangchangzzz <1015312525@qq.com>
2017-03-11 14:09:27 +0800

    完成树3

---
+++ src/binary3/main.js
@@ -0,0 +1,54 @@
+'use strict';
+
+(function(window){
+
+    var remove=document.getElementById('remove');
+    var add=document.getElementById('add');
+    var text=document.getElementById('text');
+    var container=document.getElementById('container');
+
+    //已选择节点数据模型
+    var selectedNode=null;
+
+    container.addEventListener('click',function(event){
+        if(event.target.nodeName.toLowerCase()==='div'){
+            selectedNode=event.target;
+            activeNode();
+        }
+        //当点击的是span元素的将其父元素突出显示
+        else if(event.target.nodeName.toLowerCase()==='span'){
+            selectedNode=event.target.parentNode;
+            activeNode();
+        }
+    });
+
+    remove.addEventListener('click',function(){
+        if(selectedNode!=null){
+            selectedNode.parentNode.removeChild(selectedNode);
+        }
+    });
+
+    add.addEventListener('click',function(){
+        if(selectedNode!=null){
+            var value=text.value.trim();
+            var div=document.createElement('div');
+            var span=document.createElement('span');
+            span.appendChild(document.createTextNode(value));
+            div.appendChild(span);
+
+            selectedNode.appendChild(div);
+        }
+    });
+
+    //将已选择节点突出显示
+    function activeNode(){
+        //重置样式
+        var nodes=container.getElementsByTagName('div');
+        for(var i=0,len=nodes.length;i<len;i++){
+            nodes[i].classList.remove('active');
+        }
+
+        selectedNode.classList.add('active');
+    }
+
+})(window);
