c98e3aa837a46140d1d4eb5b3dd7883e7ba20933
jiangchangzzz <1015312525@qq.com>
2017-03-22 13:08:28 +0800

    兼容了火狐浏览器

--- src/scrollInfinite/main.js
+++ src/scrollInfinite/main.js
@@ -1,52 +1,62 @@
 'use strict';
 
-(function(){
+(function () {
 
-    var list=document.querySelector('#list');
-    const num=30;   //每次向后端获取数据的数量
-    var current=0;   //下次获取数据的起始序号，序号从0开始
+    var list = document.querySelector('#list');
+    const num = 30; //每次向后端获取数据的数量
+    var current = 0; //下次获取数据的起始序号，序号从0开始
+
+    var isAddItem = false; //是否正在添加列表
 
     //获取数据渲染列表
-    function render(){
-        getdata(num,current).then(function(data){
-            //创建一个元素块
-            var block=document.createDocumentFragment();
-            data.forEach(function(item){
-                var li=document.createElement('li');
-                li.innerText=item;
-                block.appendChild(li);
+    function render() {
+        if (!isAddItem) {
+            isAddItem=true;
+            getdata(num, current).then(function (data) {
+                //创建一个元素块
+                var block = document.createDocumentFragment();
+                data.forEach(function (item) {
+                    var li = document.createElement('li');
+                    li.innerText = item;
+                    block.appendChild(li);
+                });
+                list.appendChild(block);
+
+                current += num;
+                isAddItem=false;
+            }).catch(function (err) {
+                console.log(err);
             });
-            list.appendChild(block);
-            current+=num;
-        }).catch(function(err){
-            console.log(err.message);
-        });
+        }
     }
 
     //模拟从后端获取数据,返回一个Promise对象
-    function getdata(num,curret){
-        return new Promise(function(resolve,reject){
-            setTimeout(function(){
-                var data=[];
-                for(var i=current;i<current+num;i++){
-                    data.push('item '+(i+1));
+    function getdata(num, curret) {
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                var data = [];
+                for (var i = current; i < current + num; i++) {
+                    data.push('item ' + (i + 1));
                 }
                 resolve(data);
-            },1000);
+            }, 1000);
         });
     }
 
     //初始化界面和绑定scoll事件处理程序
-    function init(){
+    function init() {
         render();
 
-        window.addEventListener('scroll',function(){
+        window.addEventListener('scroll', function () {
             //计算滚动条到底部的距离
-            var body=document.body;
-            var bottom=body.scrollHeight-body.scrollTop-window.innerHeight;
+            var totalHeight = document.body.scrollHeight;  //元素的总高度
+            var scrollTop=document.documentElement.scrollTop || document.body.scrollTop;   //被隐藏上方像素   
+            var clientHeight=document.documentElement.clientHeight;   //浏览器视口高度
 
-            //TODO此处应该改为一个范围值，但是暂时不知道怎么处理重复渲染的问题
-            if(bottom===0){
+            var bottom = totalHeight-scrollTop-clientHeight;
+            //console.log(bottom);
+            //已解决重复渲染问题
+            if (bottom < 100) {
                 render();
             }
         });
