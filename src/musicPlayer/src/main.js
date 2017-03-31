5e86d5d649d47303c2b7f8eeab326d4fcc981a8a
jiangchangzzz <1015312525@qq.com>
2017-03-31 16:33:01 +0800

    完成豆瓣音乐播放器

--- src/musicPlayer/src/main.js
+++ src/musicPlayer/src/main.js
@@ -4,7 +4,14 @@ var musics=[
     {
         name: 'what is love',
         path: 'what is love.mp3',
-        artist: 'Rader'
+        artist: 'Haddaway',
+        img: 'what is love.png'
+    },
+    {
+        name: 'Mi Mi Mi',
+        path: 'Mi Mi Mi.mp3',
+        artist: 'Serebro',
+        img: 'Mi Mi Mi.jpg'
     }
 ];
 
@@ -23,76 +30,129 @@ var musics=[
 
     var play=document.getElementById('play');
     var next=document.getElementById('next');
+    var status=document.getElementById('status');
+
+    var img=document.getElementById('music-img');
 
     var current=0;   //当前音乐序号
 
+    //加载index序号音乐和封面
     function loadMusic(index){
         var ready=musics[index];
 
         music.src='music/'+ready.path;
         name.innerText=ready.name;
         artist.innerText=ready.artist;
+
+        img.src='image/'+ready.img;
     }
 
+    //加载下一首音乐
+    function nextMusic(){
+        current++;
+        if(current>=musics.length){
+            current=0;
+        }
+        loadMusic(current);
+    }
+
+    //格式化当前时间
     function timeToString(second){
         var m=Math.floor(second/60);
         var s=Math.round(second%60);
-        if(m===0){
-            return '00:'+s;
+        return format(m)+':'+format(s);
+    }
+
+    function format(num){
+        if(num===0){
+            return '00';
+        }
+        else if(num/10<1){
+            return '0'+num;
         }
         else{
-            return m+':'+s; 
+            return num.toString();
         }
     }
 
     function init(){
-        music.addEventListener('loadedmetadata',function(){
-            time.innerText=timeToString(music.duration);
-        });
-
+        //改变音乐进度条
         music.addEventListener('timeupdate',function(){
             time.innerText=timeToString(music.currentTime);
-            var percent=music.currentTime/music.duration;
-            musicBar.style.width=musicBar.parentNode.offsetWidth*percent+'px';
+            var percent=Math.round(music.currentTime/music.duration*100);
+            musicBar.style.width=percent+'%';
         });
 
+        //改变音量进度条
         music.addEventListener('volumechange',function(){
-            volBar.style.width=volBar.parentNode.offsetWidth*music.volume+'px';
+            volBar.style.width=music.volume*100+'%';
         });
 
+        //音乐结束后播放下一首
+        music.addEventListener('ended',function(){
+           nextMusic();
+           music.play();
+        });
+
+        //播放或暂停音乐
         play.addEventListener('click',function(){
             if(music.paused || music.ended){
                 music.play();
                 this.classList.remove('glyphicon-play');
                 this.classList.add('glyphicon-pause');
+                this.title='暂停';
             }
             else{
                 music.pause();
                 this.classList.remove('glyphicon-pause');
                 this.classList.add('glyphicon-play');
+                this.title='播放';
             }
         });
 
+        //点击音乐进度条
         musicProgress.addEventListener('click',function(event){
             var left=this.getBoundingClientRect().left;
             var precent=(event.pageX-left)/this.offsetWidth;
             music.currentTime=precent*music.duration;
         });
 
+        //点击音量进度条
         volProgress.addEventListener('click',function(event){
             var left=this.getBoundingClientRect().left;
             var precent=(event.pageX-left)/this.offsetWidth;
             music.volume=Math.round(precent*10)/10;
         });
 
+        //加载下一曲，并自动播放
         next.addEventListener('click',function(){
-            current++;
-            if(current>=musics.length){
-                current=0;
+            //下一曲时保持音乐的播放和暂停状态
+            if(music.paused){
+                nextMusic();
             }
-            loadMusic(current);
+            else{
+                nextMusic();
+                music.play();
+            }
         });
 
+        //切换循环播放
+        status.addEventListener('click',function(){
+            if(music.loop){
+                music.loop=false;
+                this.classList.remove('glyphicon-refresh');
+                this.classList.add('glyphicon-align-justify');
+                this.title='列表循环';
+            }
+            else{
+                music.loop=true;
+                this.classList.remove('glyphicon-align-justify');
+                this.classList.add('glyphicon-refresh');
+                this.title='单曲循环';
+            }
+        });
+
+        //加载第一首音乐
         loadMusic(0);
     }
     init();
