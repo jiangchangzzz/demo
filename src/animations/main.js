7279a77246df656c7517c8f8ba8251aab5310a1d
jc <1015312525@qq.com>
2017-05-23 15:57:10 +0800

    完成CSS3动画特效

---
+++ src/animations/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function(){
+
+    var wrap=document.getElementById('wrap');
+    wrap.addEventListener('click',function(){
+        wrap.classList.add('cover');
+    });
+
+})();
